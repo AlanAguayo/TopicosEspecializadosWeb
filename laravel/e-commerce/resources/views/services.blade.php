d05e81ffce9f00ae123b3b7f279ab05ffea79597
alan <alan@debian.com>
2023-08-30 12:11:39 -0600

    primera clase

---
+++ laravel/e-commerce/resources/views/services.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Services</h1>
+    <ul>
+        <li>Web Pages</li>
+        <li>Information Systems</li>
+        <li>Server Administration</li>
+        <li>Network design</li>
+        <li>Mobile Apps</li>
+    </ul>
+</body>
+</html>
