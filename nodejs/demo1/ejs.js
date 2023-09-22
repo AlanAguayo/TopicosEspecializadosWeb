0fdbd67aca88a0fc5d3c533b16469aea1a346200
alan <alan@debian.com>
2023-09-22 16:18:51 -0600

    Uso de templates PugJS y EJS

---
+++ nodejs/demo1/ejs.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.get('/products', function(req, res) {
+    var products = [
+        { name: 'product1', sale_price: "10"},
+        { name: 'product2', sale_price: "20"},
+        { name: 'product3', sale_price: "30"}
+    ];
+
+    res.render('pages/products', {
+        products: products,
+    });
+});
+
+app.listen(3000);
+console.log('Puerto: 3000');
