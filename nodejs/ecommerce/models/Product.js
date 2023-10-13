6032fbe89dd9e47d83dd8040dcc11dc55cdaccdb
alan <alan@debian.com>
2023-10-13 11:26:53 -0600

    Respaldo

--- nodejs/ecommerce/models/Product.js
+++ nodejs/ecommerce/models/Product.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 require("./Category");
 
 const productSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    price: Number,
-    color: String,
-    size: String,
-    quantity: Number,
-    image: [String],
+    id: {type: Number, required: true},
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    color: {type: String, required: true},
+    size: {type: String, required: true},
+    quantity: {type: Number, required: true, min: 0, max:1000},
+    image: {type: [String], required: true},
     category:  {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
